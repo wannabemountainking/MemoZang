7ff72bb11079651402ca8440b7d46829b74423d3
wannabemountainking <kdy0096@gmail.com>
2026-02-22 14:50:35 +0900

    Core Data Provider / ViewModel

--- MemoZang/ViewModel/MemoZangViewModel.swift
+++ MemoZang/ViewModel/MemoZangViewModel.swift
@@ -22,5 +22,20 @@ final class MemoZangViewModel: ObservableObject {
         self.provider = MemoProvider.shared
         self.context = provider.newContext
         
+        if let memoZang, let existingMemo = provider.exist(context: self.context, memoZang: memoZang) {
+            self.memoZang = existingMemo
+            self.isNew = false
+        } else {
+            self.memoZang = MemoZang.empty(context: self.context)
+            self.isNew = true
+        }
+    }
+    
+    private func save() {
+        do {
+            try self.context.save()
+        } catch {
+            print("ERROR SAVING IN CORE DATA: \(error)")
+        }
     }
 }
